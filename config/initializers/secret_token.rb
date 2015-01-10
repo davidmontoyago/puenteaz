d10c7343de834f75940f82ae01d550e2bb81ec8f
David Montoya <dmontoya@thoughtworks.com>
2015-01-10 17:53:58 -0500

    app scaffolding and integration of fatfree crm as an engine

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Puenteaz::Application.config.secret_token = '33b24c4fa6b302092b92ed937ab64f354e37669b9133117c114243ef0ca79ec74b471529d4f333894d8418a0250f0d6940ce87e51dff70cf0c2f1f1919864e20'
